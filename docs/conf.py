c1183a80392bf330741b5e2ba2415d980db03867
Kim Wittenburg <codello@wittenburg.kim>
2021-12-26 17:13:09 +0100

    Add GitHub link

--- docs/conf.py
+++ docs/conf.py
@@ -13,13 +13,13 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+from datetime import datetime
 
 # -- Project information -----------------------------------------------------
 
 project = 'Karman'
 author = 'Codello, TheMcs000'
-copyright = f'2021, {author}.'
+copyright = f'{datetime.now().year} {author}.'
 
 # -- General configuration ---------------------------------------------------
 
@@ -43,8 +43,6 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_baseurl = 'https://karman.readthedocs.io/'
-sitemap_url_scheme = "{lang}{version}{link}"
 html_title = "Karman Documentation"
 html_theme = 'sphinxawesome_theme'
 html_last_updated_fmt = None
@@ -54,13 +52,12 @@ html_copy_source = False
 # html_logo = ...
 # html_favicon = ...
 
+html_collapsible_definitions = True
+html_permalinks_icon = "<span>#</span>"
 html_awesome_docsearch = False
 html_theme_options = {
     "show_scrolltop": False,
-    # "extra_header_links": {
-    #     "Docs": "/index",
-    #     "About": "/about",
-    # },
+    "extra_header_links": True,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,

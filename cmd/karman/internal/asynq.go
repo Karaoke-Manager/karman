48e4b90e3dfb09b9c23a4cd8f51ca5ae63bbb00f
Kim Wittenburg <kim@codello.dev>
2024-03-16 18:31:30 +0100

    Fix signaling on Windows

--- cmd/karman/internal/asynq.go
+++ cmd/karman/internal/asynq.go
@@ -3,7 +3,7 @@ package internal
 import (
 	"fmt"
 	"log/slog"
-	"syscall"
+	"os"
 
 	"github.com/hibiken/asynq"
 )
@@ -22,41 +22,44 @@ func AsynqLogLevel(level slog.Level) asynq.LogLevel {
 
 // AsynqLogger is an implementation of asynq.Logger backed by a slog.Logger.
 // This logger implements the Fatal method by sending a termination signal.
-type AsynqLogger slog.Logger
+type AsynqLogger struct {
+	Logger *slog.Logger     // underlying logger
+	Sig    chan<- os.Signal // termination signal
+}
 
 // Debug logs a message at Debug level.
 func (l *AsynqLogger) Debug(args ...any) {
 	for _, line := range args {
-		(*slog.Logger)(l).Debug(fmt.Sprintf("%s", line))
+		l.Logger.Debug(fmt.Sprintf("%s", line))
 	}
 }
 
 // Info logs a message at Info level.
 func (l *AsynqLogger) Info(args ...any) {
 	for _, line := range args {
-		(*slog.Logger)(l).Info(fmt.Sprintf("%s", line))
+		l.Logger.Info(fmt.Sprintf("%s", line))
 	}
 }
 
 // Warn logs a message at Warning level.
 func (l *AsynqLogger) Warn(args ...any) {
 	for _, line := range args {
-		(*slog.Logger)(l).Warn(fmt.Sprintf("%s", line))
+		l.Logger.Warn(fmt.Sprintf("%s", line))
 	}
 }
 
 // Error logs a message at Error level.
 func (l *AsynqLogger) Error(args ...any) {
 	for _, line := range args {
-		(*slog.Logger)(l).Error(fmt.Sprintf("%s", line))
+		l.Logger.Error(fmt.Sprintf("%s", line))
 	}
 }
 
 // Fatal logs a message at Error level.
-// Additionally Fatal sends a SIGTERM to the current process, leading to the program's termination.
+// Additionally Fatal sends a SIGINT to the current process, leading to the program's termination.
 func (l *AsynqLogger) Fatal(args ...any) {
 	for _, line := range args {
-		(*slog.Logger)(l).Error(fmt.Sprintf("%s", line))
+		l.Logger.Error(fmt.Sprintf("%s", line))
 	}
-	_ = syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
+	l.Sig <- os.Interrupt
 }
